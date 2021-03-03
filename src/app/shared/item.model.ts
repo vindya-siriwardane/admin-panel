b621d82d99e2b78a3406a62b859d185ae603efd7
vindya-siriwardane <vindygbv6@gmail.com>
2021-03-03 13:12:23 +0530

    other category field added

--- src/app/shared/item.model.ts
+++ src/app/shared/item.model.ts
@@ -3,6 +3,7 @@ export class Item {
     id : string;
     code : string;
     category : string;
+    otherCategory : string;
     name : string;
     quantity : number;
     price : number;
--- src/app/stock/item/item.component.ts
+++ src/app/stock/item/item.component.ts
@@ -3,6 +3,7 @@ import { ItemService } from 'src/app/shared/item.service';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { Item } from 'src/app/shared/item.model';
 
 @Component({
   selector: 'app-item',
@@ -11,23 +12,55 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ItemComponent implements OnInit {
 
+  itemList: Item[];
+  public code : string;
+
   constructor(public itemService: ItemService,
     private firestore: AngularFirestore,
     private toastr: ToastrService) { }
 
   ngOnInit() {
     this.resetForm();
+    this.itemService.getData().subscribe(actionArray => {
+      this.itemList = actionArray.map(item => {
+        return {
+          id: item.payload.doc.id,
+          ...item.payload.doc.data() as Item
+        };
+      })
+
+    });
+
   }
 
   onSubmit(itemForm: NgForm) {
-    let data = Object.assign({}, itemForm.value);
-    delete data.id;
-    if (itemForm.value.id == null)
-      this.firestore.collection('items').add(data);
-    else
-      this.firestore.doc('items/' + itemForm.value.id).update(data);
-    this.resetForm(itemForm);
-    this.toastr.success('Successfully added to the stock ! ', 'Stock Update');
+    if (document.getElementById("otherCategoryId").style.display == "flex") { 
+      let data = Object.assign({}, itemForm.value);
+      console.log(data);
+      data.category = data.otherCategory;
+      delete data.id;
+      if (itemForm.value.id == null)
+        this.firestore.collection('items').add(data);
+      else
+        this.firestore.doc('items/' + itemForm.value.id).update(data);
+      this.resetForm(itemForm);
+      this.toastr.success('Successfully added to the stock ! ', 'Stock Update');
+    } 
+    else 
+    {
+      let data = Object.assign({}, itemForm.value);
+      delete data.id;
+      // delete data.code;
+
+      if (itemForm.value.id == null && itemForm.value.code)
+        this.firestore.collection('items').add(data);
+      else
+        this.firestore.doc('items/' + itemForm.value.id).update(data);
+      
+        this.resetForm(itemForm);
+      this.toastr.success('Successfully added to the stock ! ', 'Stock Update');
+    }
+
 
   }
 
@@ -39,6 +72,7 @@ export class ItemComponent implements OnInit {
       id: null,
       code: '',
       category: '',
+      otherCategory: '',
       name: '',
       quantity: 0,
       price: 0,
@@ -46,19 +80,29 @@ export class ItemComponent implements OnInit {
 
   }
 
-  getCategory(code) {
-    if (!code) {
-      console.log("nothing here");
+  // getCategory(code) {
+  //   if (!code) {
+  //     console.log("nothing here");
+
+  //   }
+  //   else {
+
+  //     console.log(code);
+  //     var itemName = (<HTMLInputElement>document.getElementById("name"))
+  //     itemName.focus();
+  //   }
+  // }
+  onOtherCategory(event: { target: { value: any; }; }) {
+    const otherCategory = event.target.value;
+
+    if (otherCategory == "Other") {
+      document.getElementById("otherCategoryId").setAttribute("style", "display: flex");
+      document.getElementById("otherCategoryIdLabel").setAttribute("style", "display: flex");
 
     }
-    else{
-      
-      console.log(code);
-      var itemName = (<HTMLInputElement>document.getElementById("name"))
-      itemName.focus();
+    else {
+      document.getElementById("otherCategoryId").setAttribute("style", "display: none");
+      document.getElementById("otherCategoryIdLabel").setAttribute("style", "display: none");
     }
-
-
-
   }
 }
